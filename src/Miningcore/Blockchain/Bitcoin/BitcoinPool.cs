ac4a0bd6a5af6427063964d9f40546e059301cc6
megabrr <megabrr@github>
2024-08-02 13:57:15 -0400

    test start diff on bitcoin familly

--- src/Miningcore/Blockchain/Bitcoin/BitcoinPool.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinPool.cs
@@ -125,7 +125,7 @@ public class BitcoinPool : PoolBase
                (context.VarDiff != null && staticDiff.Value >= context.VarDiff.Config.MinDiff ||
                    context.VarDiff == null && staticDiff.Value > context.Difficulty))
             {
-                context.VarDiff = null; // disable vardiff
+                // context.VarDiff = null; // disable vardiff // Do not disable to change static diff to start diff.
                 context.SetDifficulty(staticDiff.Value);
 
                 logger.Info(() => $"[{connection.ConnectionId}] Setting static difficulty of {staticDiff.Value}");
